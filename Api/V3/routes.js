d33773c7cd0f8ff74c3f0fd6a5a9c2eb049af560
keshavsoft <nknnkumar@yahoo.com>
2026-05-08 15:28:43 +0530

    some del

--- Api/V3/routes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { router as routerFromItemsTable } from "./ItemsTable/end-points.js";
-
-const router = express.Router();
-router.use('/ItemsTable', routerFromItemsTable);
-
-export { router };
